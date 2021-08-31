0f48ad3593dc681f3420ce0c84d09348b698cbdf
JamesDoane <ijamesdoaneok@gmail.com>
2021-08-31 14:12:53 -0600

    error on purpose in line 22 of server.js to test rollbar functionality

--- server.js
+++ server.js
@@ -19,6 +19,8 @@ app.get('/' , (req, res) => {
     res.sendFile(path.join(__dirname, '/public/index.html'))
 })
 
+app.get('/yeehaw', thisismadeup())
+
 const port = process.env.PORT || 666
 
 app.listen(port, () => console.log("yeehaw ride em cowboy on 666"))
